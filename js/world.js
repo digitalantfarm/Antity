e76814c0e9bd564afaf86760a4c577b47d5673df
Japh <japhie@gmail.com>
2016-12-24 00:35:49 +1100

    Workers and WebGL

--- js/world.js
+++ js/world.js
@@ -7,16 +7,19 @@ let Container = PIXI.Container
   , BlurFilter = PIXI.filters.BlurFilter;
 
 class World {
-  constructor() {
+  constructor(workerScript) {
+    this.workerScript = workerScript;
     this.unitOfTime = 1000 / 60;
-    this.antities = {};
-    this.elements = {};
+    this.workers = {};
+    this.sprites = {};
     this.dimensions = {
       width: $(window).width(),
       height: $(window).height()
     };
 
     this.createWorld();
+
+    this.startWorker();
   }
 
   createWorld() {
@@ -30,43 +33,11 @@ class World {
 
     this.stage = new Container();
 
-    let antity = new Graphics();
-    antity.beginFill(0x00aaff);
-    antity.lineStyle(1,0x00aaff, 1);
-    antity.drawCircle(0, 0, 8);
-    antity.endFill();
-    antity.position.set(this.dimensions.width / 2, this.dimensions.height / 2);
-    let antityBlur = new BlurFilter();
-    antityBlur.blur = 3;
-    antity.filters = [antityBlur];
-
-    this.antity = antity;
-
-    this.stage.addChild(this.antity);
-
-    let byproduct = new Graphics();
-    byproduct.beginFill(0xffffff);
-    byproduct.lineStyle(1,0xffffff, 1);
-    byproduct.drawCircle(0, 0, 2);
-    byproduct.endFill();
-    byproduct.position.set(this.dimensions.width / 2, this.dimensions.height / 2);
-    let byproductBlur = new BlurFilter();
-    byproductBlur.blur = 1;
-    byproduct.filters = [byproductBlur];
-
-    this.byproduct = byproduct;
-
-    this.stage.addChild(this.byproduct);
-
     this.animate();
   }
 
   animate() {
     requestAnimationFrame(this.animate.bind(this));
-
-    this.antity.position.x -= 0.5;
-    this.antity.position.y -= 0.5;
-
     this.renderer.render(this.stage);
   }
 
@@ -92,50 +63,59 @@ class World {
   }
 
   addAntity(elementObject) {
-    let newElement = $('<div />');
-    newElement.attr({id: 'antity-' + elementObject.ID});
-    newElement.addClass('antity');
-    //newElement.addClass('birth');
-    newElement.offset(elementObject.offset);
+    let antity = new Graphics();
+    antity.beginFill(0x00aaff);
+    antity.lineStyle(1,0x00aaff, 1);
+    antity.drawCircle(0, 0, 8);
+    antity.endFill();
+    antity.position.set(elementObject.offset.left, elementObject.offset.top);
+    let antityBlur = new BlurFilter();
+    antityBlur.blur = 3;
+    antity.filters = [antityBlur];
 
-    this.elements[elementObject.ID] = newElement;
-    $('#world').append(this.elements[elementObject.ID]);
+    this.sprites[elementObject.ID] = antity;
+
+    this.stage.addChild(this.sprites[elementObject.ID]);
   }
 
   moveAntity(elementObject) {
-    let el = $('#antity-' + elementObject.ID);
-    el.offset(elementObject.offset);
+    this.sprites[elementObject.ID].position.set(elementObject.offset.left, elementObject.offset.top);
   }
 
   killAntity(elementObject) {
     if (!elementObject.isAlive) {
-      $('#antity-' + elementObject.ID).remove();
+      this.stage.removeChild(this.sprites[elementObject.ID]);
       console.log('Antity dead.');
       this.workers[elementObject.ID].postMessage(elementObject);
     }
   }
 
   addByproduct(elementObject) {
-    let newElement = $('<div />');
-    newElement.attr({id: 'byproduct-' + elementObject.ID});
-    newElement.addClass('byproduct');
+    let byproduct = new Graphics();
+    byproduct.beginFill(0xffffff);
+    byproduct.lineStyle(1,0xffffff, 1);
+    byproduct.drawCircle(0, 0, 2);
+    byproduct.endFill();
+    byproduct.position.set(elementObject.offset.left, elementObject.offset.top);
+    let byproductBlur = new BlurFilter();
+    byproductBlur.blur = 1;
+    byproduct.filters = [byproductBlur];
     if (elementObject.fertile) {
-      newElement.addClass('fertile');
+      byproduct.tint = 0xdd0033;
     }
-    newElement.offset(elementObject.offset);
 
-    this.elements[elementObject.ID] = newElement;
-    $('#world').append(this.elements[elementObject.ID]);
+    this.sprites[elementObject.ID] = byproduct;
+
+    this.stage.addChild(this.sprites[elementObject.ID]);
   }
 
   fadeByproduct(elementObject) {
-    let el = $('#byproduct-' + elementObject.ID);
-    el.css({opacity: elementObject.opacity});
+    this.sprites[elementObject.ID].alpha = elementObject.opacity;
   }
 
   killByproduct(elementObject) {
     if (!elementObject.isAlive) {
-      $('#byproduct-' + elementObject.ID).remove();
+      this.stage.removeChild(this.sprites[elementObject.ID]);
       this.workers[elementObject.parentAntityId].postMessage(elementObject);
     }
   }
@@ -170,14 +150,13 @@ class World {
 
 var world = new World('js/worker.js');
 
-/*
-$(document).click(function(e) {
-  let spawnLocation = { left: 0, top: 0 };
+function click2create() {
+  $(document).click(function(e) {
+    let spawnLocation = { left: 0, top: 0 };
 
-  spawnLocation.left = e.offsetX;
-  spawnLocation.top = e.offsetY;
+    spawnLocation.left = e.offsetX;
+    spawnLocation.top = e.offsetY;
 
-  //antities.push(new Antity(spawnLocation));
-  world.startWorker(spawnLocation);
-});
-*/
+    world.startWorker(spawnLocation);
+  });
+}
